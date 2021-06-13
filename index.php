2bc0533e75f1779df6402e3afa4a4ba12d64118a
raihanow <46749148+raihanow@users.noreply.github.com>
2021-06-13 17:32:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,94 +15,96 @@
 
  	<h3>TABLE CUSTOMER (mysqli_fetch_array)</h3>
 
+<!-- TABLE CUSTOMER -->
  	<table border="1" cellpadding="10" cellspacing="">
  		<tr>
- 			<th>NAMA_PENERIMA</th>
- 			<th>NO_PESANAN</th>
+ 			<th>NO PESANAN</th>
+ 			<th>NAMA PENERIMA</th>
  			<th>TANGGAL</th>
- 			<th>NAMA_BARANG</th>
+ 			<th>NAMA BARANG</th>
  			<th>ALAMAT</th>
  		</tr>
 
  	<?php foreach ($customer as $b) : ?>
 
  		<tr>
- 			<td><?= $b["NAMA_PENERIMA"]?></td>
- 			<td><?= $b["NO_PESANAN"]?></td>
- 			<td><?= $b["TANGGAL"]?></td>
- 			<td><?= $b["NAMA_BARANG"]?></td>
- 			<td><?= $b["ALAMAT"]?></td>
+ 			<td><?= $b["no_pesanan"]?></td>
+ 			<td><?= $b["nama_penerima"]?></td>
+ 			<td><?= $b["tanggal"]?></td>
+ 			<td><?= $b["nama_barang"]?></td>
+ 			<td><?= $b["alamat"]?></td>
  		</tr>
 
  	<?php endforeach; ?>
  	</table>
 
- 		<!-- ============================================================== -->
+ 		<!-- ============================================================== --><br>
 
  		<h3>TABLE PENJUAL (mysqli_fetch_array)</h3>
 
+<!-- TABLE PENJUAL -->
  	<table border="1" cellpadding="10" cellspacing="">
  		<tr>
- 			<th>NO_PESANAN</th>
- 			<th>NAMA_BARANG</th>
+ 			<th>NO PESANAN</th>
+ 			<th>NAMA BARANG</th>
  			<th>HARGA</th>
  			<th>QTY</th>
  		</tr>
 
  	<?php foreach ($penjual as $b) : ?>
  		<tr>
- 			<td><?= $b["NO_PESANAN"]?></td>
- 			<td><?= $b["NAMA_BARANG"]?></td>
- 			<td><?= $b["HARGA"]?></td>
- 			<td><?= $b["QTY"]?></td>
+ 			<td><?= $b["no_pesanan"]?></td>
+ 			<td><?= $b["nama_barang"]?></td>
+ 			<td><?= $b["harga"]?></td>
+ 			<td><?= $b["qty"]?></td>
  		</tr>
  	<?php endforeach; ?>
  	</table>
 
- 		<!-- ============================================================== -->
+ 		<!-- ============================================================== --><br>
 
  	<h3>TABLE INNER JOIN (mysqli_fetch_assoc)</h3>
 
  	<table border="1" cellpadding="10" cellspacing="">
  		<tr>
- 			<th>NAMA_PENERIMA</th>
- 			<th>NO_PESANAN</th>
+ 			<th>NO PESANAN</th>
+ 			<th>NAMA PENERIMA</th>
  			<th>TANGGAL</th>
- 			<th>NAMA_BARANG</th>
+ 			<th>NAMA BARANG</th>
  			<th>ALAMAT</th>
  		</tr>
 
  	<?php foreach ($inner_join as $b) : ?>
 
  		<tr>
- 			<td><?= $b["NAMA_PENERIMA"]?></td>
- 			<td><?= $b["NO_PESANAN"]?></td>
- 			<td><?= $b["TANGGAL"]?></td>
- 			<td><?= $b["NAMA_BARANG"]?></td>
- 			<td><?= $b["ALAMAT"]?></td>
+ 			<td><?= $b["no_pesanan"]?></td>
+ 			<td><?= $b["nama_penerima"]?></td>
+ 			<td><?= $b["tanggal"]?></td>
+ 			<td><?= $b["nama_barang"]?></td>
+ 			<td><?= $b["alamat"]?></td>
  		</tr>
 
  	<?php endforeach; ?>
  	</table>
 
- 		<!-- ============================================================== -->
+ 		<!-- ============================================================== --><br>
 
  		<h3>TABLE LEFT OUTER JOIN (mysqli_fetch_assoc)</h3>
 
  	<table border="1" cellpadding="10" cellspacing="">
  		<tr>
- 			<th>NO_PESANAN</th>
- 			<th>NAMA_BARANG</th>
+ 			<th>NO PESANAN</th>
+ 			<th>NAMA BARANG</th>
  			<th>HARGA</th>
  			<th>QTY</th>
  		</tr>
 
  	<?php foreach ($outer_join as $b) : ?>
  		<tr>
- 			<td><?= $b["NO_PESANAN"]?></td>
- 			<td><?= $b["NAMA_BARANG"]?></td>
- 			<td><?= $b["HARGA"]?></td>
- 			<td><?= $b["QTY"]?></td>
+ 			<td><?= $b["no_pesanan"]?></td>
+ 			<td><?= $b["nama_barang"]?></td>
+ 			<td><?= $b["harga"]?></td>
+ 			<td><?= $b["qty"]?></td>
  		</tr>
  	<?php endforeach; ?>
  	</table>
